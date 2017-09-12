dd710a5a0ee4f7e50681caee2463268501d2de22
mprambadi <mprambadi@gmail.com>
2017-09-12 16:57:50 +0700

    add mapss

--- app/component/Mapss.js
+++ app/component/Mapss.js
@@ -1,36 +1,75 @@
-import MapView from 'react-native-maps';
 import React, { Component } from 'react';
-import {Dimensions, StyleSheet} from 'react-native';
-
-export default class Maps extends Component {
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+export default class MapExample extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+    };
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      },
+    (error) => console.log(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      }
+    );
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
   render() {
     return (
       <MapView
-        style={styles.map}
-        region={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-        }}
+        provider={ PROVIDER_GOOGLE }
+        style={ styles.container }
+        showsUserLocation={ true }
+        region={ this.state.region }
+        onRegionChange={ region => this.setState({region}) }
+        onRegionChangeComplete={ region => this.setState({region}) }
       >
-    </MapView>
+        <MapView.Marker
+          coordinate={ this.state.region }
+        />
+      </MapView>
     );
   }
 }
-
-let { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 'auto',
-    width: 'auto',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    flex: 1,
-    width: width,
-    height: height,
-  },
+    height: '100%',
+    width: '100%',
+  }
 });
