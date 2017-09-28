5782f37694d5da33ebb0487fc22eae3a02c54a1c
mprambadi <mprambadi@gmail.com>
2017-09-28 19:51:49 +0700

    add location

--- app/component/Header.js
+++ app/component/Header.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import React, { Component } from 'react'
+import { Header, Left, Body, Right, Button, Icon, Title } from 'native-base'
 export default class HeaderTop extends Component {
-  render() {
+  render () {
     return (
-        <Header color={'#96281B'}>
-          <Body>
-            <Title>Begal Detector</Title>
-          </Body>
-          <Right>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Right>
-        </Header>
-    );
+      <Header color={'#96281B'}>
+        <Body>
+          <Title>Begal Detector</Title>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Icon name='menu' />
+          </Button>
+        </Right>
+      </Header>
+    )
   }
 }
--- app/component/Maps.js
+++ app/component/Maps.js
@@ -1,35 +1,39 @@
-import React, { Component } from 'react';
-import { 
+import React, { Component } from 'react'
+import {
   View,
-  AsyncStorage,
-  } from 'react-native';
-import { Content } from 'native-base';
-import Navigation from './Navigation';
-import Header from './Header';
-import axios from 'axios';
-
+  AsyncStorage
+} from 'react-native'
+import { Content } from 'native-base'
+import Navigation from './Navigation'
+import Header from './Header'
+import axios from 'axios'
 
 export default class Maps extends Component {
-  componentDidMount(){
+  constructor (props) {
+    super()
+    this.state = {
+      loading: true
+    }
+  }
+  
+  componentDidMount () {
     axios.post('http://c362ae30.ngrok.io/api/login', {
       'username': 'aditia',
-      'password': 'apaaja'       
+      'password': 'apaaja'
     })
-    .then( response => {
-      
-      AsyncStorage.setItem('accesToken', response.data.token.access_token);
-      
-    })
-    .catch( error => console.log(error));
+      .then(response => {
+        AsyncStorage.setItem('accesToken', response.data.token.access_token)
+      })
+      .catch(error => console.log(error))
   }
 
-  render() {
+  render () {
     return (
       <View>
         <Header />
         <Content />
         <Navigation />
       </View>
-    );
+    )
   }
 }
