20a2f772de8dd301207121b111c7c10d0bd51a30
Arnaz <arnazmursid@gmail.com>
2017-11-28 15:18:13 +0700

    add initial state and maps reducer function

--- app/component/Maps/reducer.js
+++ app/component/Maps/reducer.js
@@ -1,3 +1,20 @@
 import { fromJS } from 'immutable';
 import { UPDATE_FIELD, UPDATE_IS_LOADING } from './constants';
 
+const initialState = fromJS({
+    fields: {
+      username: 'aditia',
+      password: 'apaaja'
+    },
+    isLoading: false
+});
+
+function mapsReducer(state = initialState, action){
+    switch (action.type){
+        case UPDATE_FIELD:
+            return state.setIn([ 'fields', action.field ], action.status);
+        case UPDATE_IS_LOADING:
+            return state.set('isLoading', action.status);
+        default: return state;
+    }
+}
