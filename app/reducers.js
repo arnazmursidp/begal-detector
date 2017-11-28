54c54b2e9f19d019e83f0f2266e277146aafd339
Arnaz <arnazmursid@gmail.com>
2017-11-28 15:20:15 +0700

    import maps reducer into global reducer

--- app/reducers.js
+++ app/reducers.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux-immutable';
 
-const rootReducers = combineReducers({});
+//import reducers
+import MapsReducer from './component/Maps/reducer';
+
+const rootReducers = combineReducers({
+    mapsReducer: MapsReducer
+});
   
 export default rootReducers;
   
