27ef01d72737cb83ee2c70224dd08257476b18fa
Arnaz <arnazmursid@gmail.com>
2017-12-08 15:17:12 +0700

    create location

--- app/component/Home.js
+++ app/component/Home.js
@@ -1,68 +1,81 @@
-import React, { Component } from 'react';
-import { Container, Header, Content, List, ListItem, Text, Separator, Card,CardItem,Left, Body   } from 'native-base';
-const roads = {
-  jalan_besar: [
-    'Pasopati jembatan layang',
-    'Kiaracondong jembatan layang',
-    'Supratman',
-    'Sudirman',
-    'Sukarno Hatta',
-    'Padjadjaran',
-    'Laswi ciparay',
-  ],
-  jalan_kecil: [
-    'PSM kiaracondong',
-    'Kebaktian Kiaracondong',
-    'Tongkeng',
-    'Aceh',
-    'Martadinata',
-    'Cihampelas',
-    'Kebon Kawung',
-    'Sukajadi',
-    'Jajaran jalan Kopo',
-    'Cilaki',
-    'Cikutera',
-    'Sekejati kiaracondong',
-    'Eyckman',
-    'Terusan Buahbatu',
-    'Jl. Banjaran',
-    'Laswi ciparay',
-    'Tamansari',
-    'Siliwangi',
-  ]
+import React, { Component } from 'react'
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Text,
+  Separator,
+  Card,
+  CardItem,
+  Left,
+  Body
+} from 'native-base'
+import { AsyncStorage, ActivityIndicator, TouchableOpacity } from 'react-native'
+import axios from 'axios'
+
+export default class Home extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      locations: [],
+      streets: [],
+      loading: true
+    }
+  }
+
+  componentDidMount () {
+    let streets = []
+    AsyncStorage.getItem('accesToken')
+      .then(token => {
+        axios({
+          method: 'get',
+          url: 'http://c362ae30.ngrok.io/api/locations',
+          headers: {'Authorization': `Bearer ${token}`}
+        })
+          .then(response => {
+            response.data.data.map(location => {
+              axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=AIzaSyAQAKtSdIqhtBCBC7fVf3yaB71ch6Gyg-Y`)
+                .then(response => {
+                  // console.log(response)
+                  streets.push(response.data.results[0].address_components[1].short_name)
+                  this.setState({ streets: streets, loading: false })
+                })
+            })
+          })
+      })
+      .catch(error => console.log('error getting token', error))
+  }
+
+  render () {
+    if (this.state.loading === false) {
+      console.log(this.state.streets)
+      return (
+        <Content>
+          <CardItem>
+            <Body>
+              <Text style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>TITIK RAWAN PERAMPASAN MOTOR DI BANDUNG</Text>
+            </Body>
+          </CardItem>
+          <Separator bordered>
+
+          </Separator>
+          { this.state.streets.map((street, key) =>
+            (
+              <TouchableOpacity key={key}>
+                <Text > {street }</Text>
+              </TouchableOpacity>
+            )
+          )}
+        </Content>
+      )
+    }
+    return (
+      <ActivityIndicator
+        size="large"
+        animations={this.state.loading}
+      />
+    )
+  }
 }
-
-const Home = () => (
-  <Content>
-      <CardItem>
-
-        <Body>
-          <Text style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>TITIK RAWAN PERAMPASAN MOTOR DI BANDUNG</Text>
-        </Body>
-
-    </CardItem>
-  <Separator bordered>
-    <Text style={{fontSize: 16}}>Jalan Besar</Text>
-  </Separator>
-  {
-    roads.jalan_besar.map((besar, i) => 
-    <ListItem key={i}>
-     <Text>{besar}</Text>
-   </ListItem>
-  )
-  }
-
-  <Separator bordered>
-    <Text style={{fontSize: 16}}>Jalan Kecil</Text>
-  </Separator>
-  {
-    roads.jalan_kecil.map((kecil, i) => 
-    <ListItem key={i}>
-     <Text>{kecil}</Text>
-   </ListItem>
-  )
-  }
-</Content>
-);
-
-export default Home;
